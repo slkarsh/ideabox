118f798e6b357f71ab5602f0943c22b28284dccf
Sara Karsh <saralkarsh@gmail.com>
2019-10-17 17:21:58 -0600

    Add check for local storage

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-var main = document.querySelector('main')
-var title = document.querySelector('.form-title-input')
-var description = document.querySelector('.form-description-input')
+// var main = document.querySelector('main')
+// var title = document.querySelector('.form-title-input')
+// var description = document.querySelector('.form-description-input')
 var quotes = [];
 var submitBtn = document.querySelector('.form-submit-btn')
 
 
+
+
+checkStorage();
+appendCards();
+
 submitBtn.addEventListener('click', createNewQuote)
+
+
+// console.log(quoteInst)
 
 function createNewQuote(e) {
     e.preventDefault();
@@ -13,6 +21,7 @@ function createNewQuote(e) {
         title: document.querySelector('.form-title-input').value, 
         description: document.querySelector('.form-description-input').value 
     })
+    console.log('quote', quote)
     quotes.push(quote);
     quote.saveToStorage(quotes)
     makeCard(quote);
@@ -22,6 +31,44 @@ function createNewQuote(e) {
 function clearInputs() {
     document.querySelector('.form-title-input').value = ''
     document.querySelector('.form-description-input').value=''
+}
+
+
+function checkStorage() {
+    if (JSON.parse(localStorage.getItem("quoteKey")).length !== 0) {
+        console.log('hi hello')
+        quotes = JSON.parse(localStorage.getItem("quoteKey")).map(quote => new Quote(quote))
+    } else {
+        console.log('goodbye')
+        quotes = []
+    }
+}
+
+function checkStorage() {
+    if (JSON.parse(localStorage.getItem("quoteKey")).length !== 0) {
+        console.log('made it')
+        quotes = JSON.parse(localStorage.getItem("quoteKey")).map(function(el) {
+            return new Quote(el)
+        })
+    } else {
+        quotes = [];
+    }
+}
+
+// function checkStorage() {
+//     if (JSON.parse(localStorage.getItem("quoteKey")) === null) {
+//         quotes=[]
+//     } else {
+//         quotes = JSON.parse(localStorage.getItem("quoteKey")).map(function(element) {
+//             return new Quote(element)
+//         })
+//     }
+// }
+
+function appendCards() {
+	for (var i = 0; i < quotes.length; i++) {
+		makeCard(quotes[i]);
+	}
 }
 
 function makeCard(quote) {
